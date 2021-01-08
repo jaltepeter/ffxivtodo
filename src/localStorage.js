a7b45f5851acfc32deb6c5175dc0cbf10841ef5b
Jeremiah Altepeter <jeremiah@altepeter.com>
2021-01-08 10:26:08 -0700

    Sort list by default

--- src/localStorage.js
+++ src/localStorage.js
@@ -4,9 +4,18 @@ import React from 'react';
 /** app imports */
 import { List } from './data/items';
 
+function getDefaultList() {
+	var list = List;
+
+	list.dailies.sort((a, b) => a.name.localeCompare(b.name));
+	list.weeklies.sort((a, b) => a.name.localeCompare(b.name));
+
+	return list;
+}
+
 export const UseStateWithLocalStorage = () => {
 	const [value, setValue] = React.useState(
-		JSON.parse(localStorage.getItem('ffxivtodos')) || List);
+		JSON.parse(localStorage.getItem('ffxivtodos')) || getDefaultList());
 
 	React.useEffect(() => {
 		localStorage.setItem('ffxivtodos', JSON.stringify(value));
