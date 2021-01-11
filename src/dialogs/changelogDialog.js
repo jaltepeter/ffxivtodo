e31f5c72766a732c6e262e05ab1c9435adc515ff
Brunhine <jeremiah@altepeter.com>
2021-01-11 10:29:48 -0700

    Update changelog

--- src/dialogs/changelogDialog.js
+++ src/dialogs/changelogDialog.js
@@ -12,11 +12,16 @@ export function ChangeLog({ isModalOpen, hideModal }) {
 			<Modal.Header closeButton>
 				<Modal.Title>FFXIV Todo Tracker Beta</Modal.Title>
 			</Modal.Header>
-			<Modal.Body>
+			<Modal.Body className='changeLog'>
 				<p><strong>Thank you for helping to test this beta version!</strong></p>
 				<p>Please report any issues or suggestions on the <a href='https://github.com/Brunhine/ffxivtodo/issues' target='blank'>GitHub issue Tracker</a></p>
 				<hr />
 				<h3>v0.4.0</h3>
+				<h4>Fixes</h4>
+				<ul>
+					<li>Completed items will now sort to the bottom of the list on a user's first visit</li>
+				</ul>
+				<h4>Enhancements</h4>
 				<ul>
 					<li>Section headers and progress bar now stick to the top of each column when scrolling</li>
 				</ul>
