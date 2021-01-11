68ccd9ea73002e9f030b689d50743413c712934e
Brunhine <jeremiah@altepeter.com>
2021-01-11 10:25:13 -0700

    Add default isCompleted to all items
    Fixes #13

--- src/data/items.js
+++ src/data/items.js
@@ -5,57 +5,67 @@ export const List = {
 			name: "Grand Company Supply",
 			type: "daily",
 			link: "https://ffxiv.consolegameswiki.com/wiki/Grand_Company#Missions",
-			tags: ["DoH"]
+			tags: ["DoH"],
+			isCompleted: false
 		},
 		{
 			name: "Grand Company Provisions",
 			type: "daily",
 			link: "https://ffxiv.consolegameswiki.com/wiki/Grand_Company#Missions",
-			tags: ["DoL"]
+			tags: ["DoL"],
+			isCompleted: false
 		},
 		{
 			name: "Daily Hunts",
 			type: "daily",
 			link: "https://ffxiv.consolegameswiki.com/wiki/The_Hunt",
-			tags: ["DoW", "DoM"]
+			tags: ["DoW", "DoM"],
+			isCompleted: false
 		},
 		{
 			name: "Duty Roulettes",
 			type: "daily",
 			link: "https://ffxiv.consolegameswiki.com/wiki/Duty_Roulette",
-			tags: ["DoW", "DoM"]
+			tags: ["DoW", "DoM"],
+			isCompleted: false
 		},
 		{
 			name: "Beast Tribe Quests",
 			type: "daily",
 			link: "https://ffxiv.consolegameswiki.com/wiki/Beast_Tribe_Quests",
-			tags: ["DoW", "DoM", "DoH", "DoL"]
+			tags: ["DoW", "DoM", "DoH", "DoL"],
+			isCompleted: false
 		},
 		{
 			name: "Retainer Ventures",
 			type: "daily",
-			link: "https://ffxiv.consolegameswiki.com/wiki/Retainer_Ventures"
+			link: "https://ffxiv.consolegameswiki.com/wiki/Retainer_Ventures",
+			isCompleted: false
 		},
 		{
 			name: "Gardening",
 			type: "daily",
-			link: "https://ffxiv.consolegameswiki.com/wiki/Gardening"
+			link: "https://ffxiv.consolegameswiki.com/wiki/Gardening",
+			isCompleted: false
 		},
 		{
 			name: "Treasure Hunt",
 			type: "daily",
-			link: "https://ffxiv.consolegameswiki.com/wiki/Treasure_Hunt"
+			link: "https://ffxiv.consolegameswiki.com/wiki/Treasure_Hunt",
+			isCompleted: false
 		},
 		{
 			name: "Mini Cactpot",
 			type: "daily",
 			link: "https://ffxiv.consolegameswiki.com/wiki/Cactpot#Mini_Cactpot",
-			tags: ["Gold Saucer"]
+			tags: ["Gold Saucer"],
+			isCompleted: false
 		},
 		{
 			name: "Squadron Missions",
 			type: "daily",
-			link: "https://ffxiv.consolegameswiki.com/wiki/Adventurer_Squadrons"
+			link: "https://ffxiv.consolegameswiki.com/wiki/Adventurer_Squadrons",
+			isCompleted: false
 		},
 	],
 	weeklies: [
@@ -63,56 +73,66 @@ export const List = {
 			name: "Weekly Hunts",
 			type: "weekly",
 			link: "https://ffxiv.consolegameswiki.com/wiki/The_Hunt",
-			tags: ["DoW", "DoM"]
+			tags: ["DoW", "DoM"],
+			isCompleted: false
 		},
 		{
 			name: "Challenge Log",
 			type: "weekly",
-			link: "https://ffxiv.consolegameswiki.com/wiki/Challenge_Log"
+			link: "https://ffxiv.consolegameswiki.com/wiki/Challenge_Log",
+			isCompleted: false
 		},
 		{
 			name: "Wondrous Tails",
 			type: "weekly",
-			link: "https://ffxiv.consolegameswiki.com/wiki/Wondrous_Tails"
+			link: "https://ffxiv.consolegameswiki.com/wiki/Wondrous_Tails",
+			isCompleted: false
 		},
 		{
 			name: "Custom Deliveries",
 			type: "weekly",
-			link: "https://ffxiv.consolegameswiki.com/wiki/Custom_Deliveries"
+			link: "https://ffxiv.consolegameswiki.com/wiki/Custom_Deliveries",
+			isCompleted: false
 		},
 		{
 			name: "Priority Squadron Mission",
 			type: "weekly",
-			link: "https://ffxiv.consolegameswiki.com/wiki/Adventurer_Squadrons"
+			link: "https://ffxiv.consolegameswiki.com/wiki/Adventurer_Squadrons",
+			isCompleted: false
 		},
 		{
 			name: "Doman Enclave Donations",
 			type: "weekly",
-			link: "https://ffxiv.consolegameswiki.com/wiki/Doman_Enclave_Reconstruction"
+			link: "https://ffxiv.consolegameswiki.com/wiki/Doman_Enclave_Reconstruction",
+			isCompleted: false
 		},
 		{
 			name: "Fashion Report",
 			type: "weekly",
 			link: "https://ffxiv.consolegameswiki.com/wiki/Fashion_Report",
-			tags: ["Gold Saucer"]
+			tags: ["Gold Saucer"],
+			isCompleted: false
 		},
 		{
 			name: "Jumbo Cactpot",
 			type: "weekly",
 			link: "https://ffxiv.consolegameswiki.com/wiki/Cactpot#Jumbo_Cactpot",
-			tags: ["Gold Saucer"]
+			tags: ["Gold Saucer"],
+			isCompleted: false
 		},
 		{
 			name: "Savage Reclears",
 			type: "weekly",
 			link: "https://ffxiv.consolegameswiki.com/wiki/Raids",
-			tags: ["DoW", "DoM"]
+			tags: ["DoW", "DoM"],
+			isCompleted: false
 		},
 		{
 			name: "Newest Alliance Raid",
 			type: "weekly",
 			link: "https://ffxiv.consolegameswiki.com/wiki/Raids",
-			tags: ["DoW", "DoM"]
+			tags: ["DoW", "DoM"],
+			isCompleted: false
 		},
 	]
 };
