582b42a26884446cda4e558053b1fde96ffffc66
Brunhine <jeremiah@altepeter.com>
2021-02-01 21:59:00 -0700

    Add IDs to all items

--- src/data/items.js
+++ src/data/items.js
@@ -2,6 +2,7 @@ export const List = {
 	version: 3,
 	dailies: [
 		{
+			id: "5e56dd4e-2dc3-49fa-a194-25a85d8c7e64",
 			name: "Grand Company Supply",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Grand_Company#Missions" },
@@ -11,6 +12,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id:"6e022584-c0c3-4d36-a3de-7b4d0bd7c554",
 			name: "Grand Company Provisions",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Grand_Company#Missions" },
@@ -20,6 +22,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "6de72c7a-4540-4a00-9015-fb3d0a0b47e2",
 			name: "Daily Hunts",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/The_Hunt" },
@@ -29,6 +32,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "45caff25-2b0b-4edc-8466-21f2be5c5799",
 			name: "Duty Roulettes",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Duty_Roulette" },
@@ -38,6 +42,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "91fe9649-a04c-4288-b396-c2094c2e97bc",
 			name: "Beast Tribe Quests",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Beast_Tribe_Quests" },
@@ -47,6 +52,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "c7fe9eaf-ba84-4e60-b92b-b2abc8a32650",
 			name: "Retainer Ventures",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Retainer_Ventures" },
@@ -56,6 +62,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "af238dca-63cd-410f-842f-663da46076a7",
 			name: "Gardening",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Gardening" },
@@ -65,6 +72,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "98086dde-c367-463c-bc5a-db6989fca49d",
 			name: "Acquire Treasure Hunt Map",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Treasure_Hunt" },
@@ -74,6 +82,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "bdfe6969-4c79-41ab-8ab2-0d10d3c49c03",
 			name: "Mini Cactpot",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Cactpot#Mini_Cactpot" },
@@ -83,6 +92,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "4dd42b1d-9bd4-4a3f-b12a-afbba99dd86f",
 			name: "Squadron Missions",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Adventurer_Squadrons" },
@@ -94,6 +104,7 @@ export const List = {
 	],
 	weeklies: [
 		{
+			id: "7f8f2cc0-afc8-4005-8542-20260b92eb6d",
 			name: "Weekly Hunts",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/The_Hunt" },
@@ -103,6 +114,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "83fc772c-ad28-4767-951f-992015af6276",
 			name: "Challenge Log",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Challenge_Log" },
@@ -112,6 +124,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "0325b49a-7f9a-4c5b-ad82-7c88785dc405",
 			name: "Wondrous Tails",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Wondrous_Tails" },
@@ -121,6 +134,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "5f8cbf2b-ac1c-4548-84f3-c6a02e1471e9",
 			name: "Custom Deliveries",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Custom_Deliveries" },
@@ -130,6 +144,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "ddafb439-7dc8-4d13-867d-564adb0c4fc9",
 			name: "Priority Squadron Mission",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Adventurer_Squadrons" },
@@ -139,6 +154,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "9d5b591f-2718-452e-8fc7-74562ed1ed1a",
 			name: "Doman Enclave Donations",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Doman_Enclave_Reconstruction" },
@@ -148,6 +164,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "b9ef263b-400e-490a-a6b6-e6988eb378a6",
 			name: "Fashion Report",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Fashion_Report" },
@@ -157,6 +174,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "7602a512-5495-41ec-a1f9-b586e1682ab1",
 			name: "Jumbo Cactpot",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Cactpot#Jumbo_Cactpot" },
@@ -166,6 +184,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "9a505c83-5d7b-4932-bfda-53b03830e081",
 			name: "Savage Reclears",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Raids" },
@@ -175,6 +194,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "801b1f78-1214-4386-951f-67356a769f7e",
 			name: "Newest Alliance Raid",
 			links: [
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Raids" },
@@ -184,6 +204,7 @@ export const List = {
 			isCompleted: false
 		},
 		{
+			id: "094bd120-c7a9-4890-a7ab-57b8f163bd87",
 			name: "Faux Hollows (Unreal Trials)",
 			links:[
 				{ source: "cgw", url: "https://ffxiv.consolegameswiki.com/wiki/Faux_Hollows"},
